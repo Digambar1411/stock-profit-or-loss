42ccf009e999a40a030c0dc8036d81c5f868a9a2
Digambar <deshawaldigambar@gmail.com>
2021-09-17 23:47:22 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 var initialPrice=document.querySelector("#initial-price");
-var quantity=document.querySelector("#quamtity");
+var stockQuantity=document.querySelector("#quantity");
 var currentPrice=document.querySelector("#current-price");
 var resultBtn=document.querySelector("#btn");
-
 var output=document.querySelector("#output");
+
+function calculateProfitLoss(){
+
+    var purchasedPrice = initialPrice.value * stockQuantity.value; 
+      var sellingPrice =currentPrice.value * stockQuantity.value;
+
+    if(purchasedPrice > sellingPrice){
+       
+        showResult("its loss");
+    }
+    else if(purchasedPrice==sellingPrice){
+        showResult("there is no loss or profit");
+    }
+    else{
+        showResult("you earned profit");
+    }
+
+}
+
+function showResult(result){
+    output.innerText=result;
+}
+
+resultBtn.addEventListener("click", calculateProfitLoss);
+
+
+
