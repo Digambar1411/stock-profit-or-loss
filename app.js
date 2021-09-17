ee17a414fa7cff6948d8d9ba3d250d223a600973
Digambar <deshawaldigambar@gmail.com>
2021-09-17 23:16:39 +0530

    selected elements using querySelector

---
+++ app.js
@@ -0,0 +1,6 @@
+var initialPrice=document.querySelector("#initial-price");
+var quantity=document.querySelector("#quamtity");
+var currentPrice=document.querySelector("#current-price");
+var resultBtn=document.querySelector("#btn");
+
+var output=document.querySelector("#output");
